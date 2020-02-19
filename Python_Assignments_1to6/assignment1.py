a86815d2b2d83f67f46d91ff61d235d00636909e
taimoor arif <taimoor722@gmail.com>
2020-02-19 16:14:58 +0500

    Done

--- Python_Assignments_1to6/assignment1.py
+++ Python_Assignments_1to6/assignment1.py
@@ -16,8 +16,8 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+
+  return f'Number of donuts: {count}' if count <=9 else f'Number of donuts: many'
 
 
 # B. both_ends
@@ -26,8 +26,8 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+
+  return s[:2]+s[-2:] if len(s) >=2 else ''
 
 
 # C. fix_start
@@ -41,7 +41,7 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  return  s[0]+s[1:].replace(s[0],'*')
 
 
 # D. MixUp
@@ -53,7 +53,7 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  return b[:2]+a[2:]+' '+a[:2]+b[2:]
 
 
 # Provided simple test() function used in main() to print
@@ -63,36 +63,36 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
 
 
 # Provided main() calls the above functions with interesting inputs,
 # using test() to check if each result is correct or not.
 def main():
-  print 'donuts'
+  print ('donuts')
   # Each line calls donuts, compares its result to the expected for that call.
   test(donuts(4), 'Number of donuts: 4')
   test(donuts(9), 'Number of donuts: 9')
   test(donuts(10), 'Number of donuts: many')
   test(donuts(99), 'Number of donuts: many')
 
-  print
-  print 'both_ends'
+
+  print ('both_ends')
   test(both_ends('spring'), 'spng')
   test(both_ends('Hello'), 'Helo')
   test(both_ends('a'), '')
   test(both_ends('xyz'), 'xyyz')
 
   
-  print
-  print 'fix_start'
+
+  print ('fix_start')
   test(fix_start('babble'), 'ba**le')
   test(fix_start('aardvark'), 'a*rdv*rk')
   test(fix_start('google'), 'goo*le')
   test(fix_start('donut'), 'donut')
 
-  print
-  print 'mix_up'
+
+  print ('mix_up')
   test(mix_up('mix', 'pod'), 'pox mid')
   test(mix_up('dog', 'dinner'), 'dig donner')
   test(mix_up('gnash', 'sport'), 'spash gnort')
